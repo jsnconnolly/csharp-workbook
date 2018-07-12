14e8f3645ece51a7b6f90177eced955af66d1974
Jason Connolly <jasonconnolly@Jasons-MacBook-Pro.local>
2018-07-11 20:55:15 -0500

    HelloWorld

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -1,12 +1,44 @@
 ﻿using System;
-
-namespace HelloWorld
+					
+public class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello Jason!");
-        }
-    }
+	public static void Main()
+	{
+		string name = "";
+		string lastname = "";
+		int age = 0;
+		int year = 7;
+		string band = "";
+		string team = "";
+		bool people = true;
+		bool f = false;
+		decimal num = 1.5m;
+		
+
+
+		
+		Console.WriteLine("Please enter your first name: ");
+		name = Console.ReadLine();
+		Console.WriteLine("Please enter your last name: ");
+		lastname = Console.ReadLine();
+		Console.WriteLine("Please enter your age: ");
+		age = Convert.ToInt32(Console.ReadLine());
+		//Console.WriteLine("Please enter 7: ");
+		//year = Convert.ToInt32(Console.ReadLine());
+		//comment: put divisibale into line 10
+		Console.WriteLine("Who is your favorite band: ");
+		band = Console.ReadLine();
+		Console.WriteLine("What sports team do you cheer for: ");
+		team = Console.ReadLine();
+		
+		
+		Console.WriteLine(@"Hello! 
+		My name is {0} {1}. 
+		I am {3} in dog years. 
+		My favorite band is {4}. 
+		I support {5}! ", name, lastname, age, year*age, band, team,);
+
+
+
+	}
 }
