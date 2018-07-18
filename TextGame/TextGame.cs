9305cfbe64f66fe62864a6e9b69f4e8839333192
Jason Connolly <jasonconnolly@Jasons-MacBook-Pro.local>
2018-07-18 12:30:31 -0500

    TextGame

--- TextGame/TextGame.cs
+++ TextGame/TextGame.cs
@@ -27,49 +27,54 @@ namespace TextGame
             int stick;
 
             if (userInput.ToLower() == "yes")
-                {
+            /*converts all inputs to lower case */
+            {
                  Console.WriteLine("You have taken the stick!");    
-                    stick = 1;
-                }
-            else{
+                 stick = 1;
+            }
+            else
+            {
                 Console.WriteLine("You did not take the stick");
                 stick = 0;
             }
         
 
-        Console.WriteLine ("As you proceed further into the cave, you see a small glowing object");
-        Console.WriteLine ("Do you approach the object? [y/n]");
+            Console.WriteLine ("As you proceed further into the cave, you see a small glowing object");
+            Console.WriteLine ("Do you approach the object? [y/n]");
         
         string chapter2 = Console.ReadLine();
 
+       
+            Console.WriteLine("You enter a dark cavern out of curiosity. It is dark and you can only make out a small stick on the floor.");
+            Console.WriteLine("Do you take it? [y/n]: ");
+       /* }
+    }
+}*/
+/*# STICK TAKEN*/
+            if (userInput.ToLower() == "yes")
+            {
+                Console.WriteLine("You have taken the stick!");
+                stick = 1;
+             }
+/*# STICK NOT TAKEN*/
+            else
+            {
+                Console.WriteLine("You did not take the stick");
+                stick = 0;
+            }
+            Console.WriteLine("As you proceed further into the cave, you see a small glowing object");
+            Console.WriteLine("Do you approach the object? [y/n]");
         }
     }
 }
 
-/* 
-Console.WriteLine("You enter a dark cavern out of curiosity. It is dark and you can only make out a small stick on the floor.")
-Console.WriteLine("Do you take it? [y/n]: "))
-   
-# STICK TAKEN
-if input ['y', 'Y', 'Yes', 'YES', 'yes']
-    Console.WriteLine("You have taken the stick!")
-    Thread.Sleep (2);
-    int stick = 1
-        
-# STICK NOT TAKEN
-else:
-    Console.WriteLine("You did not take the stick")
-    stick = 0
-Console.WriteLine ("As you proceed further into the cave, you see a small glowing object")
-ch2 = str(input("Do you approach the object? [y/n]"))
-
-# APPROACH SPIDER
-if ch2 in ['y', 'Y', 'Yes', 'YES', 'yes']:
-    Console.WriteLine ("You approach the object...")
+/*# APPROACH SPIDER
+        if (userInput.ToLower() == "yes")
+        Console.WriteLine ("You approach the object...")
     time.sleep(2)
-    Console.WriteLine ("As you draw closer, you begin to make out the object as an eye!")
+        Console.WriteLine ("As you draw closer, you begin to make out the object as an eye!")
     time.sleep(1)
-    Console.WriteLine ("The eye belongs to a giant spider!")
+         Console.WriteLine ("The eye belongs to a giant spider!")
     ch3 = str(input("Do you try to fight it? [Y/N]"))
 
     # FIGHT SPIDER
@@ -77,45 +82,45 @@ if ch2 in ['y', 'Y', 'Yes', 'YES', 'yes']:
 
         # WITH STICK
         if stick == 1:
-            Console. WriteLine ("You only have a stick to fight with!")
-            Console.WriteLine ("You quickly jab the spider in it's eye and gain an advantage")
+            Console. WriteLine ("You only have a stick to fight with!");
+            Console.WriteLine ("You quickly jab the spider in it's eye and gain an advantage");
             time.sleep(2)
-            Console.WriteLine ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            Console.WriteLine ("                  Fighting...                   ")
-            Console.WriteLine ("   YOU MUST HIT ABOVE A 5 TO KILL THE SPIDER    ")
-            Console.WriteLine ("IF THE SPIDER HITS HIGHER THAN YOU, YOU WILL DIE")
-            Console.WriteLine ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            Console.WriteLine ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine ("                  Fighting...                   ");
+            Console.WriteLine ("   YOU MUST HIT ABOVE A 5 TO KILL THE SPIDER    ");
+            Console.WriteLine ("IF THE SPIDER HITS HIGHER THAN YOU, YOU WILL DIE");
+            Console.WriteLine ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             time.sleep(2)
             fdmg1 = int(random.randint(3, 10))
             edmg1 = int(random.randint(1, 5))
-            Console.WriteLine ("you hit a", fdmg1)
-            Console.WriteLine ("the spider hits a", edmg1)
+            Console.WriteLine ("you hit a", fdmg1);
+            Console.WriteLine ("the spider hits a", edmg1);
             time.sleep(2)
 
             if edmg1 > fdmg1:
-                Console.WriteLine ("The spider has dealt more damage than you!")
+                Console.WriteLine ("The spider has dealt more damage than you!");
                 complete = 0
                 return complete
 
             elif fdmg1 < 5:
-                Console.WriteLine ("You didn't do enough damage to kill the spider, but you manage to escape")
+                Console.WriteLine ("You didn't do enough damage to kill the spider, but you manage to escape");
                 complete = 1
                 return complete
 
             else:
-                Console.WriteLine ("You killed the spider!")
+                Console.WriteLine ("You killed the spider!");
                 complete = 1
                 return complete
 
         # WITHOUT STICK
         else:
-            Console.WriteLine ("You don't have anything to fight with!")
-            Console.WriteLinesleep(2)
-            Console.WriteLine ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            Console.WriteLine ("                  Fighting...                   ")
-            Console.WriteLine ("   YOU MUST HIT ABOVE A 5 TO KILL THE SPIDER    ")
-            Console.WriteLine ("IF THE SPIDER HITS HIGHER THAN YOU, YOU WILL DIE")
-            Console.WriteLine ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            Console.WriteLine ("You don't have anything to fight with!");
+            Console.WriteLinesleep(2);
+            Console.WriteLine ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine ("                  Fighting...                   ");
+            Console.WriteLine ("   YOU MUST HIT ABOVE A 5 TO KILL THE SPIDER    ");
+            Console.WriteLine ("IF THE SPIDER HITS HIGHER THAN YOU, YOU WILL DIE");
+            Console.WriteLine ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             time.sleep(2)
             fdmg1 = int(random.randint(1, 8))
             edmg1 = int(random.randint(1, 5))
