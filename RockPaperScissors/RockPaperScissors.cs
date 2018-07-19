0468f4a1de2139fa141210fcc62c4e62fa435b75
Jason Connolly <jasonconnolly@Jasons-MacBook-Pro.local>
2018-07-19 12:11:45 -0500

    RockPaperScissors

--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -5,21 +5,59 @@ namespace RockPaperScissors
     class Program
     {
         public static void Main()
+        
         {
-            Console.WriteLine("Enter hand 1:");
+
+            Console.WriteLine("Rock, Paper or Sissors?:");
             string hand1 = Console.ReadLine().ToLower();
-            Console.WriteLine("Enter hand 2:");
-            string hand2 = Console.ReadLine().ToLower();
+            
+            string hand2 = null;
+            Random rnd = new Random();
+                int rps = rnd.Next(0,2); 
+            if (rps == 0)
+            {
+                hand2 = "rock";
+            }
+            else if (rps == 1)
+            {
+                hand2 = "paper"; 
+            }
+            else if (rps == 2)
+            {
+                hand2 = "sissors";
+            }
+                 
+            
+            Console.WriteLine("Computer played:");
             Console.WriteLine(CompareHands(hand1, hand2));
 
             // leave this command at the end so your program does not close automatically
-            Console.ReadLine();
+        
         }
         
         public static string CompareHands(string hand1, string hand2)
         {
-            // Your code here
-            return hand1 + ' ' + hand2;
+            /*Random rnd = new Random();
+                int rock = rnd.Next(0,2); 
+                int paper = rnd.Next(0,2);   
+                int sissors = rnd.Next(0,2);*/   
+            
+            string win = null;
+            if (hand1 == hand2)
+            {
+               win = "TIE!";
+            }
+            else if (hand1 == "paper" && hand2 == "rock" || hand1 == "rock" && hand2 == "sissors" || hand1 == "sissors" && hand2 == "paper")
+            {
+                win = "You Win!";
+            }
+            
+            else if (hand1 == "rock" && hand2 == "paper" || hand1 == "sissors" && hand2 == "rock" || hand1 == "paper" && hand2 == "sissors")
+            {
+                win = "Computer Wins!";
+            }
+           
+            return win;
         }
     }
 }
