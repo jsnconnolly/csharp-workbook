bfecd20d6380aee0efdd7c019d44b7a850958246
Jason Connolly <jasonconnolly@Jasons-MacBook-Pro.local>
2018-07-19 13:18:31 -0500

    RockPaperScissors

--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -14,6 +14,7 @@ namespace RockPaperScissors
             string hand2 = null;
             Random rnd = new Random();
                 int rps = rnd.Next(0,2); 
+
             if (rps == 0)
             {
                 hand2 = "rock";
