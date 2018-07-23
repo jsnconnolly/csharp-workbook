af04404a7a3d4509d86aad372a5aed1b82eeb32f
Jason Connolly <jasonconnolly@Jasons-MacBook-Pro.local>
2018-07-23 12:35:39 -0500

    PigLatin

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -4,18 +4,49 @@ namespace PigLatin
 {
     class Program
     {
-        public static void Main()
-        {
-            // your code goes here
+        static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a word/sentence to convert to PigLatin:");
+        string sentence = Console.ReadLine();
+        string pigLatin = ToPigLatin(sentence);
+        Console.WriteLine(pigLatin);
+    }
 
-            // leave this command at the end so your program does not close automatically
-            Console.ReadLine();
-        }
-        
-        public static string TranslateWord(string word)
+    static string ToPigLatin (string sentence)
+    {            
+        string firstLetter,
+               restOfWord,
+               vowels = "AEIOUaeio";
+        int currentLetter;
+
+        foreach (string word in sentence.Split())
         {
-            // your code goes here
-            return word;
+            firstLetter = sentence.Substring(0, 1);
+            restOfWord = sentence.Substring(1, sentence.Length - 1);
+            currentLetter = vowels.IndexOf(firstLetter);
+
+            if (currentLetter == -1)
+            {
+                sentence = restOfWord + firstLetter + "ay";
+            }
+            else
+            {
+                sentence = word + "yay";
+            }
+        }
+    
+            return sentence;
         }
     }
 }
+            //leave this command at the end so your program does not close automatically
+            //Console.ReadLine();
+        //}
+        
+        //public static string TranslateWord(string word)
+        //{
+            // your code goes here
+           // return word;
+        //}
+   // }
+//}
