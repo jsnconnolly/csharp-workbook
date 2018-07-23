021352ceaec53b1bef3406b4891962c335e8ddc5
Jason Connolly <jasonconnolly@Jasons-MacBook-Pro.local>
2018-07-23 15:03:43 -0500

    RockPaperScissors

--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -2,13 +2,16 @@
 
 namespace RockPaperScissors
 {
-    class Program
+    class Program           
     {
+        public static int score1 = 0;
+        public static int score2 = 0;  //default values
+
         public static void Main()
         
         {
-            Console.WriteLine("Hello []");
-            Console.WriteLine("Enter Rock, Paper or Scissors?");
+            Console.WriteLine("Hello");
+            Console.WriteLine("Enter Rock, Paper or Scissors");
             string hand1 = Console.ReadLine().ToLower();
 
             string hand2 = null;
@@ -30,10 +33,21 @@ namespace RockPaperScissors
             }
                  
             
-            Console.WriteLine("Computer played:");
-            Console.WriteLine(hand2.ToString());
+            
             Console.WriteLine(CompareHands(hand1, hand2));
-        
+            Console.WriteLine("Your Score:" + score1);
+            Console.WriteLine("Computer Score:" + score2);
+            Console.WriteLine("Play again Yes or No");
+            string hand = Console.ReadLine().ToLower();
+            if (hand=="yes")
+            {
+                Main(); //recurssion
+            }
+            else
+            {
+                Console.Read();
+            }
+
         }
         
         public static string CompareHands(string hand1, string hand2)
@@ -42,19 +56,34 @@ namespace RockPaperScissors
             string win = null;
             if (hand1 == hand2)
             {
-               win = "TIE!";
+                Console.WriteLine("Computer played:");
+                Console.WriteLine(hand2.ToString());
+                win = "TIE!";
             }
             else if (hand1 == "paper" && hand2 == "rock" || hand1 == "rock" && hand2 == "scissors" || hand1 == "scissors" && hand2 == "paper")
             {
+                Console.WriteLine("Computer played:");
+                Console.WriteLine(hand2.ToString());
+                score1++;
                 win = "You Win!";
             }
             
             else if (hand1 == "rock" && hand2 == "paper" || hand1 == "scissors" && hand2 == "rock" || hand1 == "paper" && hand2 == "scissors")
             {
+                Console.WriteLine("Computer played:");
+                Console.WriteLine(hand2.ToString());
+                score2++;
                 win = "Computer Wins!";
+            }
+            else
+            {
+                //returns line without computer play
+                Console.WriteLine("Invalid Selection");
+                Main();
             }
            
             return win;
+            
             
         }
     }
