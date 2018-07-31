8e668faab86ab92f545f2a8335f4ca7068ab8705
Jason Connolly <jasonconnolly@Jasons-MacBook-Pro.local>
2018-07-30 19:58:26 -0500

    GradeBook

---
+++ GradeBook/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+/*Req:
+1. implement a student to grade array dictionary 
+(ie Dictionary<String,int[]> or Dictionary<String,String[]>
+2. for practice, you could implement the student input mechanism as a Stack<String>//
