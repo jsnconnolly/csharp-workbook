935ffd4224128a042caecdcdf6f680942d076b8f
Jason Connolly <jasonconnolly@Jasons-MacBook-Pro.local>
2018-08-13 17:21:23 -0500

    Checkpoint1

--- Checkpoint1/Checkpoint1.cs
+++ Checkpoint1/Checkpoint1.cs
@@ -6,13 +6,45 @@ namespace Checkpoint1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Select A Program");
+            Console.WriteLine("1 - divBy3");
+            Console.WriteLine("2 - sumCalc");
+            Console.WriteLine("3 - factoral");
+            Console.WriteLine("4 - guessNum");
+            Console.WriteLine("5 - findMax");
+            string input = Console.ReadLine();
+            
+            if (input == "1")
+            {
+                divBy3();
+            }
+            
+            else if(input == "2")
+            {
+                //the next function
+            }
+
+        }
+
+        static void divBy3()
+        {
         for (int i = 1; i <= 100; i++)
         
             if (i % 3 == 0)
         { 
             Console.WriteLine(i);
         }
+        }
+        static void sumCalc()
+        {
+        // next function here
+        }
+    
+
+
+
+
+        
     }
-}
 }
 
