2ff6d7fbafe22c97aa45ece579c83d65b5837028
Jason Connolly <jasonconnolly@Jasons-MacBook-Pro.local>
2018-08-27 20:31:34 -0500

    Linq

--- Linq/Linq.cs
+++ Linq/Linq.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Linq
+namespace StudentList
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	public class Program
+	{
+		public static void Main()
+		{
+		List<Student> students = new List<Student>();
+		
+		students.Add(new Student("Chris", "123-456-7891", "123 Delany", -2990));
+		students.Add(new Student("Kevin", "512-222-2222", "435 Carolyn", -2500));
+		students.Add(new Student("Victoria", "512-827-8498", "701 Brazos St", 0));
+		students.Add(new Student("Luke", "555-555-5555", "451 Brody Ln", -500));
+		
+		var 
+		}
+
+	}
+	
+	public class Student
+	{
+		public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+		public int Balance { get; set; }
+		
+		public Student (string name, string phone, string address, int balance)
+		{
+			Name = name;
+			Phone = phone;
+			Address = address;
+			Balance = balance;
+		}
+	}
 }
