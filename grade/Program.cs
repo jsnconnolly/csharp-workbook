db7698d9a5039b5ff64426c65cd24ff75dcf8696
Jason Connolly <jasonconnolly@Jasons-MacBook-Pro.local>
2018-09-12 18:28:17 -0500

    09week-2

---
+++ grade/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace grade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
