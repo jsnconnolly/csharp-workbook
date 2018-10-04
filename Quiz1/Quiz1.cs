51e4146168271b64840dc151729a99a132108aef
Jason Connolly <jasonconnolly@Jasons-MacBook-Pro.local>
2018-10-03 21:10:33 -0500

    week one updates

---
+++ Quiz1/Quiz1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Quiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Make Notes!!!");
+            Console.WriteLine("Welcome Back!");
+            //use of the modules, be carefull using negatives
+            Console.WriteLine((7%(-3)));
+            Console.WriteLine((7%(3)));
+
+            int x = 7;
+            int y = 7;
+
+            Console.WriteLine(x++);
+            Console.WriteLine(++y);
+        }
+    }
+}
