ab0b1fb83ede47d61756059be87ea8cdd938300f
Jason Connolly <jasonconnolly@Jasons-MacBook-Pro.local>
2018-10-08 08:05:05 -0500

    TextGame.cs

--- TextGame/TextGame.cs
+++ TextGame/TextGame.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace TextGame
 {
@@ -6,7 +7,16 @@ namespace TextGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Console.WriteLine("Welcome to the cavern of secrets!");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Thread.Sleep(3000);
+        //time pause method in C#(millsec)
+
+        Console.WriteLine("You enter a dark cavern out of curiosity. It is dark and you can only make out a small stick on the floor.");
+
+
         }
     }
 }
