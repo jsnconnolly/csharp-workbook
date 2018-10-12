b9630cddd62791e26dd4f174ea627ff396bbfbcd
Jason Connolly <jasonconnolly@Jasons-MacBook-Pro.local>
2018-10-12 10:50:13 -0500

    RockPaperScissors.cs

--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -2,24 +2,61 @@
 
 namespace RockPaperScissors
 {
-    class Program
+    class Program           
     {
         public static void Main()
-        {
-            Console.WriteLine("Enter hand 1:");
-            string hand1 = Console.ReadLine().ToLower();
-            Console.WriteLine("Enter hand 2:");
-            string hand2 = Console.ReadLine().ToLower();
-            Console.WriteLine(CompareHands(hand1, hand2));
-
-            // leave this command at the end so your program does not close automatically
-            Console.ReadLine();
-        }
         
+        {	        
+            Console.WriteLine("Enter hand 1:");	
+            Console.WriteLine("Rock, Paper or Sissors?:");
+            string hand1 = Console.ReadLine().ToLower();
+            Console.WriteLine("Enter hand 2:");	            
+            string hand2 = null;
+            Random rnd = new Random();
+            int rps = rnd.Next(0,2); 
+            if (rps == 0)
+            {
+                hand2 = "rock";
+            }
+            else if (rps == 1)
+            {
+                hand2 = "paper"; 
+            }
+            else if (rps == 2)
+            {
+                hand2 = "sissors";
+            }
+                 
+            
+            Console.WriteLine("Computer played: {0}", hand2);
+            Console.WriteLine(CompareHands(hand1, hand2));
+            // leave this command at the end so your program does not close automatically
+            Console.ReadLine();	        
+        }	        
+        	        
         public static string CompareHands(string hand1, string hand2)
-        {
-            // Your code here
-            return hand1 + ' ' + hand2;
-        }
-    }
-}
+        {	
+            Random rnd = new Random();
+            int rock = rnd.Next(0,2); 
+        
+             
+            
+            string win = null;
+            if (hand1 == hand2)
+            {
+               win = "TIE!";
+            }
+            else if (hand1 == "paper" && hand2 == "rock" || hand1 == "rock" && hand2 == "sissors" || hand1 == "sissors" && hand2 == "paper")
+            {
+                win = "You Win!";
+            }
+            
+            else if (hand1 == "rock" && hand2 == "paper" || hand1 == "sissors" && hand2 == "rock" || hand1 == "paper" && hand2 == "sissors")
+            {
+                win = "Computer Wins!";
+            }
+           
+            return win;
+        }	        
+    }	    
+}	
