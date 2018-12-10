ce3ec2381ec85a51e7c15281315b1ecf9c510d5a
Jason Connolly <jasonconnolly@Jasons-MacBook-Pro.local>
2018-12-10 16:09:46 -0600

    Projects

---
+++ Today/Today.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Today
+{
+    class Today
+    {
+        static void Main(string[] args)
+        {
+           String file = @"/Users/jasonconnolly/Desktop/words.txt";
+           String[] lines = File.ReadAllLines(file);
+           //reading all lines and putting in an array
+
+           int lineNo =0;
+           foreach(String line in lines){
+               lineNo +=1;
+               Console.WriteLine(lineNo+"+line");
+            //reading each line and then writing to the console
+           }
+            
+            String copy= @"/Users/jasonconnolly/Desktop/.txt";
+            File.WriteAllLines(copy, lines);
+            //creates new file and puts into new file
+        }
+    }
+}
---
+++ Week9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
